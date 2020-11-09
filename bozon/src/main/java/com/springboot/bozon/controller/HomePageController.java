e12a7a41217dcaeb0ceefd290da0efdca750279a
mialyshev <Har13Jor23>
2020-11-09 21:39:58 +0300

    redesigned html pages

--- bozon/src/main/java/com/springboot/bozon/controller/HomePageController.java
+++ bozon/src/main/java/com/springboot/bozon/controller/HomePageController.java
@@ -1,10 +1,15 @@
 package com.springboot.bozon.controller;
 
 import com.springboot.bozon.model.Post;
+import com.springboot.bozon.model.User;
 import com.springboot.bozon.repository.UserRepository;
 import com.springboot.bozon.service.PostService;
 import com.springboot.bozon.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,9 +35,12 @@ public class HomePageController {
     }
 
     @GetMapping("/post")
-    public String findAll(Model model) {
+    public String findAll(Model model, @AuthenticationPrincipal UserDetails currentUser) {
+        User user = (User) userRepository.findByUsername(currentUser.getUsername());
+        model.addAttribute("currentUser", user);
         List<Post> posts = postService.findAll();
         model.addAttribute("posts", posts);
+
         return "post-list";
     }
 
